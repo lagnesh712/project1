36503489c92d5cef07fa40ed5aab795d77595b60
lagnesh712 <lagneshchouhan@gmail.com>
2022-08-23 18:58:11 +0530

    app.py project first commit

---
+++ static/python.py
@@ -0,0 +1,1 @@
+print("hello world this world is so beautiful")
---
+++ testpython.py
@@ -0,0 +1,8 @@
+import os
+
+# mkdir = os.mkdir("test_dir")
+# os.rmdir("test_dir")
+list=os.listdir()
+print(list)
+cpu=os.cpu_count()
+print(cpu)
